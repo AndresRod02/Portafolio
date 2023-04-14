d49ceadd0032f307c2fc38e77c6f90472dfef19d
Andrés Rodríguez <artdavid2022@gmail.com>
2023-04-14 16:34:50 -0500

    Cv download

--- assets/download.js
+++
@@ -1,7 +0,0 @@
-const btnDescargar = document.getElementById('download-btn');
-const enlaceDescarga = document.getElementById('download-link');
-
-btnDescargar.addEventListener('click', () => {
-  enlaceDescarga.click();
-});
-alert("Hola ")
